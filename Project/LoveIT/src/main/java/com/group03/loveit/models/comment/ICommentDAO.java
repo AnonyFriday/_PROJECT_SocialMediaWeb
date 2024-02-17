25718085d13ba85851a29463fb2e64c338aff21f
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2024-02-17 18:40:59 +0700

    Update ICommentDAO.java

--- Project/LoveIT/src/main/java/com/group03/loveit/models/comment/ICommentDAO.java
+++ Project/LoveIT/src/main/java/com/group03/loveit/models/comment/ICommentDAO.java
@@ -1,10 +1,15 @@
 package com.group03.loveit.models.comment;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 /**
  * @author Nhat
  */
 public interface ICommentDAO {
     public CommentDTO getCommentById(long id);
+    public CompletableFuture<List<CommentDTO>> getCommentsByPost(long postId);
+    public CompletableFuture<List<CommentDTO>> getChildComments(long id);
     public void insertComment(CommentDTO comment);
     public void updateComment(CommentDTO comment);
     public void deleteComment(long id);
